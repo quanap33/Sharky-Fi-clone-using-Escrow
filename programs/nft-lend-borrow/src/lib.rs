3a46cb33dd7002a554485542946d7d1d491e3550
quanap33 <154326137+quanap33@users.noreply.github.com>
2023-12-19 22:24:03 +0700

    Update lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("2ZvxGFUi1dGww3EBPSrwvcjHcnrFocrB4JcUzevRrY36");
 
 #[program]
 pub mod nft_lend_borrow {
